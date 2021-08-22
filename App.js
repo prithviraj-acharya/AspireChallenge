125000457846150366cdd1f9c552138a2b1a248d
Prithviraj Acharya <46262787+prithviraj-acharya@users.noreply.github.com>
2021-08-23 00:55:06 +0530

    Minor change for babel.config

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import React, {useEffect} from 'react';
 import StackNavigator from './src/navigators/StackNavigator';
 import {useDispatch, useSelector} from 'react-redux';
